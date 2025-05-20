90a48b860a85b6479901493c041f36178d3c6e8a
pSus365 <suotr12@gmail.com>
2025-05-20 17:40:59 +0200

    reading clients data from DB to MainPage with responsive tables added

--- LegalOfficeManager/LegalOfficeManagerApp/Controllers/LegalOfficeEntriesController.cs
+++ LegalOfficeManager/LegalOfficeManagerApp/Controllers/LegalOfficeEntriesController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LegalOfficeManagerApp.Data;
+using LegalOfficeManagerApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LegalOfficeManagerApp.Controllers
 {
     public class LegalOfficeEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db; 
+        public LegalOfficeEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<LegalOfficeEntry> objLegalOfficeEntryList = _db.LegalOfficeEntries.ToList();
+            return View(objLegalOfficeEntryList);
+
         }
     }
 }
