3f4ee2fb5771fde50419c61a1912a9ea39b35e0b
Michal <michal.siudut.ms@gmail.com>
2025-05-22 18:11:05 +0200

    editing profile added

--- LegalOfficeManager/LegalOfficeManagerApp/Controllers/ProfileManagmentController.cs
+++ LegalOfficeManager/LegalOfficeManagerApp/Controllers/ProfileManagmentController.cs
@@ -27,5 +27,38 @@ namespace LegalOfficeManagerApp.Controllers
 
             return View(userData); // przekaż model do widoku
         }
+        [HttpPost]
+        public async Task<IActionResult> SaveChanges(ApplicationUser model)
+        {
+            if (!ModelState.IsValid)
+                return View("Index", model);
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return NotFound();
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+            user.Gender = model.Gender;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                TempData["ProfileUpdated"] = "Your changes have been updated!";
+                return RedirectToAction("Index");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            TempData["ProfileUpdated"] = "Twoje zmiany zostały zapisane!";
+            return RedirectToAction("Index");
+
+           //return View("Index", model);
+        }
+
     }
 }
