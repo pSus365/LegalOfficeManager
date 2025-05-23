20f39aa749872caec7d3b0b1392371a46fbc36de
pSus365 <suotr12@gmail.com>
2025-05-23 14:05:03 +0200

    Cases CRUD operations added

--- LegalOfficeManager/LegalOfficeManagerApp/Controllers/LegalOfficeEntriesController.cs
+++ LegalOfficeManager/LegalOfficeManagerApp/Controllers/LegalOfficeEntriesController.cs
@@ -41,5 +41,68 @@ namespace LegalOfficeManagerApp.Controllers
             
             return View(obj);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == null)
+            {
+                return NotFound();
+            }
+
+            LegalOfficeEntry? legalOfficeEntry = _db.LegalOfficeEntries.Find(id);
+            
+            if (legalOfficeEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(legalOfficeEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LegalOfficeEntry obj)
+        {
+            //server side data validation
+            if (obj != null && obj.Name.Length < 3)
+            {
+                ModelState.AddModelError("Name", "Name must be at least 3 characters long.");
+            }
+
+            if (ModelState.IsValid) // saving data only when the model state is valid (everything went good)
+            {
+                _db.LegalOfficeEntries.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == null)
+            {
+                return NotFound();
+            }
+
+            LegalOfficeEntry? legalOfficeEntry = _db.LegalOfficeEntries.Find(id);
+
+            if (legalOfficeEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(legalOfficeEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(LegalOfficeEntry obj)
+        {
+            _db.LegalOfficeEntries.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
